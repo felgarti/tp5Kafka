9c2169246c42cb4b4eb0c3086154d55243b44d2d
felgarti <elgartifatima@gmail.com>
2023-02-17 16:01:27 +0100

    streams

--- src/main/java/org/sid/demospringcloudkafka/services/PageEventService.java
+++ src/main/java/org/sid/demospringcloudkafka/services/PageEventService.java
@@ -1,5 +1,9 @@
 package org.sid.demospringcloudkafka.services;
 
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Grouped;
+import org.apache.kafka.streams.kstream.KStream;
 import org.sid.demospringcloudkafka.entities.PageEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
@@ -35,6 +39,15 @@ public class PageEventService {
             input.setName("Page event");
             input.setUser("UUUU");
             return  input;
+        };
+    }
+@Bean
+    public Function<KStream<String , PageEvent> , KStream<String , Long>>  KStreamFunction(){
+        return ( input)->{
+            return input.filter((k,v)->v.getDuration()>100).map((k,v)->new KeyValue<>(v.getName(),0L))
+                    .groupBy((k,v)->k,Grouped.with(Serdes.String() , Serdes.Long()))
+                    .count()
+                    .toStream();
         };
     }
 
